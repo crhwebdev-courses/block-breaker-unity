8d55265dbb8754a4d76b007619d58db355ca70f7
Carl Hain <crhain@gmail.com>
2020-07-14 16:29:26 -0500

    When ball hits a block it will destroy the block

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        Debug.Log(collision.gameObject.name);
+    }
+}
