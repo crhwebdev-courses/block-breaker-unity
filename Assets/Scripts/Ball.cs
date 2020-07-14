9cdfc03314dc6b68190d5725c4c744c59a33ef00
Carl Hain <crhain@gmail.com>
2020-07-14 13:51:28 -0500

    Add ability to launch ball with left mouse click to start game

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,12 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Ball : MonoBehaviour
 {   // config params
     [SerializeField] private Paddle paddle1;
+    [SerializeField] private float xPush = 2f;
+    [SerializeField] private float yPush = 15f;
 
     // state
     private Vector2 paddleToBallVector;
-
+    private bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,26 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        // stick ball position to paddle position
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
     }
