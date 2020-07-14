e97b5e193d4354ca044d4f05628f150ee8f303bf
Carl Hain <crhain@gmail.com>
2020-07-14 13:39:08 -0500

    Ball script ensures that Ball position is stuck to paddle at start of game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{   // config params
+    [SerializeField] private Paddle paddle1;
+
+    // state
+    private Vector2 paddleToBallVector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
