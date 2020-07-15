669c77840d0de1033439e6dacd6a5d066b308c78
Carl Hain <crhain@gmail.com>
2020-07-15 12:04:34 -0500

    Add game speed option; remove sceneLoader game object and place sceneLoader on Level object

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f, 10f)] [SerializeField] private float gameSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;    
+    }
+}
