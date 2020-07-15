b89ec334e28135abe0197c79bc33695872b2fa0a
Carl Hain <crhain@gmail.com>
2020-07-15 13:05:49 -0500

    Add score display with updating score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,13 +1,21 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
     // config params
+    [SerializeField] private TextMeshProUGUI scoreText;
     [Range(0.1f, 10f)] [SerializeField] private float gameSpeed = 1f;
     [SerializeField] private int pointsPerBlockDestroyed = 83;
 
     // state variables
     [SerializeField] private int currentScore = 0;
+
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,5 +26,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
