9d88f08254084c8af3d327138ed2ab564164ce2b
Carl Hain <crhain@gmail.com>
2020-07-14 20:52:32 -0500

    Add Level object to Level 2 and script that counts number of breakable blocks on level

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,7 +2,16 @@
 
 public class Block : MonoBehaviour
 {
-    [SerializeField] private AudioClip breakSound;  
+    [SerializeField] private AudioClip breakSound;
+
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {       
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private int breakableBlocks; // Serialized for debugging purposes
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+   
+}
